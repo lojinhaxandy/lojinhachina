16b9381af94c3089b50356fdd0713921e615eaf8
lojinhaxandy <xandynetflix1552@gmail.com>
2025-06-10 15:05:57 -0400

    Primeiro commit lojinhachina

---
+++ data/products.ts
@@ -0,0 +1,10 @@
+export const products = [
+  {
+    id: 'cpf-china',
+    name: 'CPF para China',
+    description: 'CPF PARA CHINAS (*Funciona na GoBank e FastBank)',
+    price: 0.25,
+    image: '/images/cpf-china.png',
+    fileUrl: '/downloads/cpf-para-china.txt',
+  },
+];
